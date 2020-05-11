c64065a54abf32cfeb31cdfc484103e65e481d13
ArturMarkov <azoveagl@gmail.com>
2020-05-11 19:52:23 +0300

    add numpy and pandas

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -25,3 +25,19 @@ import pandas as pd
 data = pd.read_csv('data\household_power_consumption.txt', sep=";", header=0)
 #data.columns = ["a", "b", "c", "etc."]
 print(data.head())
+
+
+% %time
+dtype = [('Active', "f4"),('Reactive',"f4"),('Voltage',"f4"),('Intensity',"f4"),('Sub_1',"i4"),('Sub_2',"i4"),('Sub_3',"i4")]
+_data = np.genfromtxt("data/household_power_consumption.txt", delimiter=';',
+                      usecols=range(2, 9), dtype=dtype, skip_header=True, missing_values=['?'])
+_d = np.loadtxt("data/household_power_consumption.txt",
+                delimiter=';', usecols=range(0, 1), dtype="U", skiprows=1)
+_t = np.loadtxt("data/household_power_consumption.txt",
+                delimiter=';', usecols=range(1, 2), dtype="U", skiprows=1)
+data = rfn.append_fields(_data, 'Date', _d, usemask=False)
+data = rfn.append_fields(data, 'Time', _t, usemask=False)
+
+
+del _d, _t
+data
